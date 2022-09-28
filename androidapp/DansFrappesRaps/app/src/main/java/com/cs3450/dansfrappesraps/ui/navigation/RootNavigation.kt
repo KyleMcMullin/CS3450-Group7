501f3a34ba969dafb93f2c60203efd80ce7df0e9
KyleMcMullin <mcmullinjkyle@gmail.com>
2022-09-27 22:17:16 -0600

    Revert "updating navigation to use nav graph and support more screens in the foyer"
    This reverts commit 81b4aae103dbb9e721adfd789592fbb798248fbd.

--- androidapp/DansFrappesRaps/app/src/main/java/com/cs3450/dansfrappesraps/ui/navigation/RootNavigation.kt
+++ androidapp/DansFrappesRaps/app/src/main/java/com/cs3450/dansfrappesraps/ui/navigation/RootNavigation.kt
@@ -15,7 +15,6 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.*
 import com.cs3450.dansfrappesraps.ui.screens.MenuScreen
 import com.cs3450.dansfrappesraps.ui.screens.SignInScreen
-import com.cs3450.dansfrappesraps.ui.screens.SignUpScreen
 
 @Composable
 fun RootNavigation() {
@@ -35,18 +34,17 @@ fun RootNavigation() {
     ){
         NavHost(
             navController = navController,
-            startDestination = Routes.foyer.route,
+            startDestination = Routes.signIn.route,
             modifier = Modifier.padding(paddingValues = it)
         ) {
-            navigation(route = Routes.foyer.route, startDestination = Routes.signIn.route){
-                composable(route = Routes.signIn.route) { SignInScreen(navHostController = navController)}
-                composable(route = Routes.signUp.route) { SignUpScreen(navController)}
-            }
-            navigation(route = Routes.app.route, startDestination = Routes.menu.route) {
-                composable(route = Routes.menu.route) {MenuScreen(navHostController = navController)}
-            }
-//            composable(route = Routes.signIn.route) { SignInScreen(navHostController = navController)}
-//            composable(route = Routes.menu.route) {MenuScreen(navController)}
+//            navigation(route = Routes.foyer.route, startDestination = Routes.signIn.route){
+//                composable(route = Routes.signIn.route) { SignInScreen(navHostController = navController)}
+//            }
+//            navigation(route = Routes.app.route, startDestination = Routes.menu.route) {
+//                composable(route = Routes.menu.route) {MenuScreen(navHostController = navController)}
+//            }
+            composable(route = Routes.signIn.route) { SignInScreen(navHostController = navController)}
+            composable(route = Routes.menu.route) {MenuScreen(navController)}
         }
     }
 //    NavHost(navController = navController, startDestination = Routes.signIn.route) {
