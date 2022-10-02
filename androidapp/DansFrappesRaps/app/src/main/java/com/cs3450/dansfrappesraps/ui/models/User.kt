0ce1d0b629bad46acb7c2ca2f0ce388299a3ee42
KyleMcMullin <mcmullinjkyle@gmail.com>
2022-10-02 14:07:00 -0600

    adding database user creation upon signup

---
+++ androidapp/DansFrappesRaps/app/src/main/java/com/cs3450/dansfrappesraps/ui/models/User.kt
@@ -0,0 +1,11 @@
+package com.cs3450.dansfrappesraps.ui.models
+
+data class User(
+    val id: String? = null,
+    val userId: String? = null,
+    val name: String? = null,
+    val email: String? = null,
+    val employee: Boolean? = false,
+    val manager: Boolean? = false,
+    val favorites: List<String>? = null
+)
