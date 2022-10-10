7fb79afdd42fe4d700c340700c228e1690999450
Andrew Wilkinson <andrew.wilkinson@usu.edu>
2022-10-10 15:39:36 -0600

    removed targetdropdown.xml

--- androidapp/DansFrappesRaps/app/src/main/java/com/cs3450/dansfrappesraps/ui/screens/MenuScreen.kt
+++ androidapp/DansFrappesRaps/app/src/main/java/com/cs3450/dansfrappesraps/ui/screens/MenuScreen.kt
@@ -3,23 +3,17 @@ package com.cs3450.dansfrappesraps.ui.screens
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
-import androidx.navigation.NavController
 import androidx.navigation.NavHostController
-import com.cs3450.dansfrappesraps.ui.repositories.UserRepository
-import kotlinx.coroutines.launch
 
 @Composable
 fun MenuScreen(navHostController: NavHostController) {
-    val scope = rememberCoroutineScope()
     Column(
         modifier = Modifier
             .fillMaxSize()
