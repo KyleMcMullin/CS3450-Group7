d099fdf0300333f6724e0d9a65e49592e5b73ef6
a02331323 <a02331323@usu.edu>
2022-10-17 16:31:06 -0600

    splash screen and adaptive logos for future use

--- androidapp/DansFrappesRaps/app/src/main/java/com/cs3450/dansfrappesraps/ui/screens/SplashScreen.kt
+++ androidapp/DansFrappesRaps/app/src/main/java/com/cs3450/dansfrappesraps/ui/screens/SplashScreen.kt
@@ -1,19 +1,24 @@
 package com.cs3450.dansfrappesraps.ui.screens
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.NavHostController
+import com.cs3450.dansfrappesraps.R
 import com.cs3450.dansfrappesraps.ui.navigation.Routes
 import com.cs3450.dansfrappesraps.ui.viewmodels.SplashScreenViewModel
 import kotlinx.coroutines.delay
@@ -36,15 +41,30 @@ fun SplashScreen(navHostController: NavHostController) {
             }
         }
     }
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.SpaceAround
-    ) {
-        Text(
-            text = "Dan's Fraps and Wraps",
-            modifier = Modifier.fillMaxWidth(),
-            style = MaterialTheme.typography.headlineLarge,
-            textAlign = TextAlign.Center
-        )
+    Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+        Column(modifier = Modifier.fillMaxSize()) {
+            Row(modifier = Modifier.fillMaxHeight(.33F).fillMaxWidth()){}
+            Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.Center) {
+                Image(
+                    painter = painterResource(id = R.drawable.logo),
+                    alignment = Alignment.Center,
+                    contentScale = ContentScale.Fit,
+                    contentDescription = "Logo" ,
+                    modifier = Modifier
+                        .size(250.dp)
+                        .clip(CircleShape)
+                        .padding(10.dp)
+                )
+
+            }
+            Row(modifier = Modifier.fillMaxSize(), verticalAlignment = Alignment.Bottom) {
+                Text(
+                    text = "Dan's Frappes & Raps",
+                    modifier = Modifier.fillMaxWidth().padding(15.dp),
+                    style = MaterialTheme.typography.headlineLarge,
+                    textAlign = TextAlign.Center
+                )
+            }
+        }
     }
 }
