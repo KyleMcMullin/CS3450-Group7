2963f32770d064960115620801e9eeae6d1430a0
a02331323 <a02331323@usu.edu>
2022-10-18 12:20:35 -0600

    Rough menu detail item view

--- androidapp/DansFrappesRaps/app/src/main/java/com/cs3450/dansfrappesraps/ui/screens/DetailMenuScreen.kt
+++ androidapp/DansFrappesRaps/app/src/main/java/com/cs3450/dansfrappesraps/ui/screens/DetailMenuScreen.kt
@@ -1,24 +1,28 @@
 package com.cs3450.dansfrappesraps.ui.screens
 
 import androidx.compose.foundation.layout.*
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
+import com.cs3450.dansfrappesraps.ui.components.IngredientItem
 import com.cs3450.dansfrappesraps.ui.components.Loader
 import com.cs3450.dansfrappesraps.ui.viewmodels.EditMenuViewModel
 import kotlinx.coroutines.async
 import kotlinx.coroutines.delay
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun DetailMenuScreen(navController: NavController, id: String?){
+fun DetailMenuScreen(navController: NavController, id: String?) {
     var viewModel: EditMenuViewModel = viewModel()
     var scope = rememberCoroutineScope()
     var state = viewModel.uiState
@@ -33,16 +37,92 @@ fun DetailMenuScreen(navController: NavController, id: String?){
     if (state.loading) {
         Spacer(modifier = Modifier.height(16.dp))
         Loader()
-    }
-    else {
+    } else {
         Column(
             modifier = Modifier
-                .fillMaxSize()
-                .padding(16.dp),
+                .fillMaxSize(),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Text(text = state.name, style = MaterialTheme.typography.headlineLarge)
+            Text(text = "Drink name", style = MaterialTheme.typography.headlineLarge)
+            Divider(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(5.dp), thickness = 3.dp
+            )
+            Text(modifier = Modifier.fillMaxWidth(), text = "Size",  style = MaterialTheme.typography.headlineMedium, textAlign = TextAlign.Left)
+            Divider(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(5.dp), thickness = 3.dp
+            )
+            Row() {
+                Column(modifier = Modifier.fillMaxWidth(.5F)) {
+                    Button(modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(3.dp), onClick = {}) {
+                        Text(
+                            modifier = Modifier.fillMaxWidth(),
+                            text = "Small",
+                            textAlign = TextAlign.Center,
+                            style = MaterialTheme.typography.headlineSmall
+                        )
+                    }
+                }
+                Column(modifier = Modifier.fillMaxWidth()) {
+                    Button(modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(3.dp), onClick = {}) {
+                        Text(
+                            modifier = Modifier.fillMaxWidth(),
+                            text = "Medium",
+                            textAlign = TextAlign.Center,
+                            style = MaterialTheme.typography.headlineSmall
+                        )
+                    }
+
+                }
+            }
+            Column(modifier = Modifier.fillMaxWidth()) {
+                Button(modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(3.dp), onClick = {}){
+                    Text(modifier = Modifier.fillMaxWidth(),
+                        text = "Large",
+                        textAlign = TextAlign.Center,
+                        style = MaterialTheme.typography.headlineSmall
+                    )
+                }
+            }
+            Row(modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 20.dp)) {
+                Text(text = "Customization", modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(3.dp), style = MaterialTheme.typography.headlineMedium)
+                Divider()
+//                Text(text = "Quantity", modifier = Modifier.fillMaxWidth(), style = MaterialTheme.typography.titleMedium,)
+            }
+
+            LazyColumn(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .fillMaxHeight(.9F)
+                    .padding(horizontal = 16.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                items(state.ingredients, key = { it.inventory?.id!! }) { ingredient ->
+                    IngredientItem(
+                        ingredient = ingredient,
+                        onMinusPressed = {viewModel.decrementIngredient(ingredient)},
+                        onPlusPressed = { viewModel.incrementIngredient(ingredient) },
+                    )
+                    Spacer(modifier = Modifier.height(8.dp))
+                }
+
+            }
+//            Button(modifier = Modifier.fillMaxWidth(), onClick = { /*TODO*/ }) {
+
+            }
         }
     }
-
 }
